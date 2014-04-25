b836d75875d7c14f53d23d748084c24ae7724735
Jake Drahos <drahos@iastate.edu>
2014-04-25 16:17:03 -0500

    Some reworked routes. Began implementing secrets for vote50

--- models.py
+++ models.py
@@ -5,11 +5,13 @@ class TeamScore(Base):
     __tablename__ = "scores"
     id = Column(Integer, primary_key=True)
     score = Column(BigInteger)
-    teamName = Column(Integer)
+    teamName = Column(String(40))
+    secret = Column(Integer)
 
-    def __init__ (self, team, score):
+    def __init__ (self, team, score, secret=0):
         self.teamName = team
         self.score = score
+        self.secret = secret
 
     def __repr__ (self):
         return '<%s : %d>' % (self.teamName, self.score)
--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 from db import db_session, init_db
 from models import TeamScore
 from sqlalchemy import desc
@@ -7,11 +8,40 @@ init_db()
 
 app = Flask(__name__)
 
-@app.route("/plus1/<teamname>")
+@app.route("/<teamname>/plus1")
 def onepoint(teamname):
     q = db_session.query(TeamScore).filter(TeamScore.teamName == teamname)
     r = q.all()
     newScore = r[0].score + 1
+    q.update({TeamScore.score : newScore})
+    print r[0]
+    print newScore
+    db_session.commit()
+    return "Team %s new score %d" % (teamname, newScore)
+
+##disabled until redis is added##
+#@app.route("/<teamname>/plus5")
+def fivepoint(teamname):
+    ###check secret from args against secret stored in redis###
+    q = db_session.query(TeamScore).filter(TeamScore.teamName == teamname)
+    r = q.all()
+    newScore = r[0].score + 5
+    q.update({TeamScore.score : newScore})
+    print r[0]
+    print newScore
+    db_session.commit()
+    return "Team %s new score %d" % (teamname, newScore)
+
+@app.route("/votefor/<teamname>")
+def votefor(teamname):
+    ###put secret in redis###
+    return renderTemplate("vote.html", TEAMNAME=teamname)
+
+@app.route("/<teamname>/plus50")
+def fiftypoint(teamname):
+    q = db_session.query(TeamScore).filter(TeamScore.teamName == teamname)
+    r = q.all()
+    newScore = r[0].score + 50
     q.update({TeamScore.score : newScore})
     print r[0]
     print newScore
