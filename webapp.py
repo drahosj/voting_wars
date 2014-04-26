bb127b9777eaf408e2d507877a96001adf8ab0e7
Jake Drahos <drahos@iastate.edu>
2014-04-26 11:14:30 -0500

    Now using redis

--- webapp.py
+++ webapp.py
@@ -1,92 +1,75 @@
 from flask import Flask, render_template
 from flask import request, redirect
+from redis import StrictRedis
 import random
-from db import db_session, init_db
-from models import TeamScore
-from sqlalchemy import desc
 
-init_db()
+r = StrictRedis(host='localhost', port=6379, db=0)
 
 app = Flask(__name__)
 
 @app.route("/<teamname>/plus1")
 def onepoint(teamname):
-    q = db_session.query(TeamScore).filter(TeamScore.teamName == teamname)
-    r = q.all()
-    newScore = r[0].score + 1
-    q.update({TeamScore.score : newScore})
-    print r[0]
-    print newScore
-    db_session.commit()
+    r.incrby("score:%s" % teamname, 1)
     return redirect("/")
-    #return "Team %s new score %d" % (teamname, newScore)
 
 @app.route("/<teamname>/plus5")
 def fivepoint(teamname):
-    q = db_session.query(TeamScore).filter(TeamScore.teamName == teamname)
-    r = q.all()
-    newScore = r[0].score + 5
-    q.update({TeamScore.score : newScore})
-    print r[0]
-    print newScore
-    db_session.commit()
+    r.incrby("score:%s" % teamname, 5)
     return redirect("/")
-    #return "Team %s new score %d" % (teamname, newScore)
 
 @app.route("/<teamname>/plus50")
 def fiftypoint(teamname):
 ##needs to check secret against stored secret in db##
-    q = db_session.query(TeamScore).filter(TeamScore.teamName == teamname)
-    r = q.all()
-    
     provided_secret = request.args.get('secret');
 
     new_secret = random.randrange(100)
 
+    expected_secret = r.get("secret:%s" % teamname)
 
-    if r[0].secret != int(provided_secret):
-        q.update({TeamScore.secret : new_secret})
+    if int(expected_secret) != int(provided_secret):
+        r.set("secret:%s" % teamname, new_secret)
+        print "Wrong Secret"
         return redirect("/")
-#        return "Wrong Secret! (provided %s, expected %d)" % (provided_secret, r[0].secret)
 
-    newScore = r[0].score + 50
-    q.update({TeamScore.score : newScore, TeamScore.secret : new_secret})
-    print r[0]
-    print newScore
-    db_session.commit()
+    r.set("secret:%s" % teamname, new_secret)
+    r.incrby("score:%s" % teamname, 50)
     return redirect("/")
-    #return "Team %s new score %d" % (teamname, newScore)
 
 @app.route("/<teamname>/votefor")
 def votefor(teamname):
 ###put secret in db and render in template###
-    q = db_session.query(TeamScore).filter(TeamScore.teamName == teamname)
     secret = random.randrange(100)
-    q.update({TeamScore.secret : secret})
-    db_session.commit()
+    r.set("secret:%s" % teamname, secret)
     return render_template("vote.html", TEAMNAME=teamname, SECRET=secret)
 
 @app.route("/supersecret/create/<teamname>")
 def create(teamname):
-    add = TeamScore(team=teamname, score=0)
-    db_session.add(add)
-    db_session.commit()
+    r.set(("score:%s" % teamname), 0)
     return "Team %s created" % teamname
 
 @app.route("/supersecret/delete/<teamname>")
 def delete(teamname):
-    q = db_session.query(TeamScore).filter(TeamScore.teamName == teamname)
-    print q
-    print q.all()
-    q.delete()
-    db_session.commit()
+    r.delete(("score:%s" % teamname))
     return "Team %s deleted" % teamname
+
+
+class Team():
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def getScore(self):
+        return self.score
 
 @app.route("/")
 def scores():
-    teamScores = db_session.query(TeamScore).order_by(desc(TeamScore.score)).all()
-    print teamScores
-    return render_template("scoreboard.html", SCORES=teamScores)
+    teams = []
+    for teamName in r.keys("score*"):
+        teamName = teamName.split(":")[1]
+        newTeam = Team(teamName, r.get("score:%s" % teamName))
+        teams.append(newTeam)
+
+    teams.sort(key=Team.getScore, reverse=True)
+    return render_template("scoreboard.html", TEAMS=teams)
 
 
 if __name__ == "__main__":
