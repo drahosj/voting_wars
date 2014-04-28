3336777a7a0f3caf950038f36bbb83f2edc022a4
Jake Drahos <drahos@iastate.edu>
2014-04-28 14:19:53 -0500

    Added comments

--- webapp.py
+++ webapp.py
@@ -7,21 +7,22 @@ r = StrictRedis(host='localhost', port=6379, db=0)
 
 app = Flask(__name__)
 
+#Array for storing valid team names. This was a last-minute hack
 valid_teams = []
 
-#@app.route("/<teamname>/plus1")
+@app.route("/<teamname>/plus1")
 def onepoint(teamname):
     if teamname in valid_teams:
         r.incrby("score:%s" % teamname, 1)
     return redirect("/")
 
-#@app.route("/<teamname>/plus5")
+@app.route("/<teamname>/plus5")
 def fivepoint(teamname):
     if teamname in valid_teams:
         r.incrby("score:%s" % teamname, 5)
     return redirect("/")
 
-#@app.route("/<teamname>/plus50")
+@app.route("/<teamname>/plus50")
 def fiftypoint(teamname):
 ##needs to check secret against stored secret in db##
     provided_secret = request.args.get('secret');
@@ -47,11 +48,14 @@ def votefor(teamname):
     r.set("secret:%s" % teamname, secret)
     return render_template("vote.html", TEAMNAME=teamname, SECRET=secret)
 
+#Uncomment to enable team creation. Not really needed
+#with valid_teams array
 #@app.route("/supersecret/create/<teamname>")
 def create(teamname):
     r.set(("score:%s" % teamname), 0)
     return "Team %s created" % teamname
 
+#Same as above
 #@app.route("/supersecret/delete/<teamname>")
 def delete(teamname):
     r.delete(("score:%s" % teamname))
@@ -83,6 +87,7 @@ if __name__ == "__main__":
     print valid_teams
     app.run("0.0.0.0")
 
+#Set up valid team names. This is a last-minute hack
 for i in range (1, 32):
     valid_teams.append("Team %d" % i)
     valid_teams.append("Team RED")
